56ddad77dd745908d46315d492ba818c7715deee
sachinchaubey <sachinchaubey205@gmail.com>
2022-09-08 13:00:31 +0530

    first commit

--- hello.java
+++ hello.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 class hello{
     public static void main(String[] args) {
         System.out.println("hello namste sachin");
--- hello2.java
+++ hello2.java
@@ -2,6 +2,7 @@
 class hello2{
     public static void main(String[] args){
         System.out.println("nmste");  
-        System.out.println("hello sachin");      
+        System.out.println("hello sachin");   
+        System.out.println("hello java");   
     }
 }
