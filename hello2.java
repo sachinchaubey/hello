a6b85cd3cad6dab378faedff7715602bc450edce
sachinchaubey <sachinchaubey205@gmail.com>
2022-09-08 12:52:09 +0530

    first commit

--- hello2.java
+++ hello2.java
@@ -1,6 +1,7 @@
 
 class hello2{
     public static void main(String[] args){
-        System.out.println("nmste");        
+        System.out.println("nmste");  
+        System.out.println("hello sachin");      
     }
 }
